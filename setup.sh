82d3bd56ab6fb35b91d617496930d93b0da774b6
nathanieljmette <nathanieljmette@arizona.edu>
2025-04-17 14:46:21 -0700

    Updated setup.sh to not delete the data directory. Also updated to not showcase the process of unzipping the download and instead replaced with a loading screen.

--- setup.sh
+++ setup.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-if [ -d "data" ]; then
-    rm -r -f "data"
-    echo -e "Removed old data materials.\n"
-fi
 
-echo -e "Generating data directory...\n"
-mkdir data
+if ! [ -d "data" ]; then
+    echo -e "Data directory not found. Generating new data directory...\n"
+    mkdir data
+fi
 
 echo -e "Downloading metadata..."
 cd data
@@ -13,16 +11,27 @@ curl -O https://os.unil.cloud.switch.ch/fma/fma_metadata.zip
 OUTPUT=`echo "f0df49ffe5f2a6008d7dc83c6915b31835dfe733  fma_metadata.zip" | sha1sum -c -`
 
 if [ "$OUTPUT" == "fma_metadata.zip: OK" ]; then
-    echo -e "fma_metadata.zip downloaded properly. Unpacking.\n"
+    echo -e "fma_metadata.zip downloaded properly. Installing."
 else
     echo -e "fma_metadata.zip downloaded improperly. Please retry.\n"
     exit
 fi
 
-unzip fma_metadata.zip
+unzip -o -q fma_metadata.zip &
+pid=$!
 
-rm fma_metadata.zip
+spin="-\|/"
+
+i=0
+while kill -0 $pid 2>/dev/null
+do
+    i=$(( (i+1) %4 ))
+    printf "\r${spin:$i:1}"
+    sleep 0.5
+done
+
+rm -f fma_metadata.zip
 
 cd ..
 
-echo -e "Finished setup. Exitting.\n\n"
+echo -e "\bFinished setup. Exitting."
